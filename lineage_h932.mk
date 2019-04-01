3b82fab2571e7ea0fdf0d5b937912c790d25ab00
SGCMarkus <markusornik@gmail.com>
2019-04-01 18:01:41 +0200

    h932: update fingerprint to 20r
    Change-Id: I806c1f9bf387ff6e6a14153db7a6410e2ba0e3c1

--- lineage_h932.mk
+++ lineage_h932.mk
@@ -15,8 +15,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="joan" \
     PRODUCT_DEVICE="joan" \
     PRODUCT_NAME="joan_tmo_us" \
-    PRIVATE_BUILD_DESC="joan_tmo_us-user 8.0.0 OPR1.170623.026 1819221128e3d release-keys"
+    PRIVATE_BUILD_DESC="joan_tmo_us-user 8.0.0 OPR1.170623.026 1900816569db7 release-keys"
 
-BUILD_FINGERPRINT="lge/joan_tmo_us/joan:8.0.0/OPR1.170623.026/1819221128e3d:user/release-keys"
+BUILD_FINGERPRINT="lge/joan_tmo_us/joan:8.0.0/OPR1.170623.026/1900816569db7:user/release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model
